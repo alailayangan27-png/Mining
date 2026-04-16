99496a13e6ed42328264ed779622590b1acfe322
alailayangan27-png <alailayangan27@gmail.com>
2026-04-16 23:00:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
 import os
 
 app = Flask(__name__)
-
-users = {}
 
 @app.route("/")
 def home():
     return "APP RUNNING ✅"
 
-@app.route("/balance", methods=["POST"])
-def balance():
-    user_id = request.json.get("user_id")
-
-    if user_id not in users:
-        users[user_id] = 1000
-
-    return jsonify({"balance": users[user_id]})
-
-@app.route("/mine", methods=["POST"])
-def mine():
-    user_id = request.json.get("user_id")
-
-    if user_id not in users:
-        users[user_id] = 1000
-
-    users[user_id] += 10
-
-    return jsonify({"balance": users[user_id]})
+# WAJIB untuk Railway
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
