0e2869d048441ac62c2a79fdca27b70848c9978d
alailayangan27-png <alailayangan27@gmail.com>
2026-04-16 23:23:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+# fake database (sementara)
+balance = 0
+
 @app.route("/")
 def home():
-    return "APP RUNNING ✅"
+    return render_template("index.html")
+
+@app.route("/mine", methods=["POST"])
+def mine():
+    global balance
+    balance += 1
+    return jsonify({"balance": balance})
+
+@app.route("/balance")
+def get_balance():
+    return jsonify({"balance": balance})
