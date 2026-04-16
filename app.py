a5e1f1e27d5ffc8d4ab56ab14dbc3cce5006e322
alailayangan27-png <alailayangan27@gmail.com>
2026-04-16 23:18:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "APP RUNNING ✅"
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
